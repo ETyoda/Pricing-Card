e11b9d79ab0892553b7d28aed17fe91e5a069f28
Yodahe <etyodahegosa21@gmail.com>
2024-09-01 23:59:44 +0300

    cardElement

--- PricingCard/src/Component/PricingCard/PricingCard.jsx
+++ PricingCard/src/Component/PricingCard/PricingCard.jsx
@@ -7,6 +7,16 @@ function PricingCard({ label, priceLabel, image, imageAlt }) {
       <div className={styles.card_image}>
         <img src={image} alt={imageAlt} />
       </div>
+      <span className={styles.card_priceLabel}>{priceLabel}</span>
+      <span>Everything in Free, Plus</span>
+      <ul className={styles.card_benefits}>
+        <li className={styles.card_benefits}>Upto 10 Users</li>
+        <li className={styles.card_benefits}>Email Support, Call Support</li>
+        <li className={styles.card_benefits}>1 Year Access</li>
+      </ul>
+      <button className={styles.card_button}>
+        
+      </button>
     </div>
   );
 }
